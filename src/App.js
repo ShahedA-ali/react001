9d37e46a19e85beae316637e31d23f96fa12b92e
AliKhalid <ali16kalid@gmail.com>
2024-06-10 08:39:42 +0430

    routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/login';
+import Register from './pages/register';
+import Main from './pages/Main';
 
 function App() {
 	const [data, setData] = useState([])
-	useEffect(() => {
-		fetch('http://127.0.0.1:8000').then((res) => res.json()).then(res => setData(res));
-	}, [])
+	// useEffect(() => {
+	// 	fetch('http://127.0.0.1:8000/login').then((res) => res.json()).then(res => setData(res));
+	// }, [])
 
 	return (
 		<div className="App">
-			{!data && <div>Loading....</div>}
-			{data && <div>{data.result}</div>}
+			<Routes>
+				<Route exact path='/login' Component={Login}></Route>
+				<Route exact path='/register' Component={Register}></Route>
+				<Route exact path='/' Component={Main}></Route>
+
+			</Routes>
 		</div>
 	);
 }
